94d0206452b80fa9bf70725156ce5259d5c2addd
Koby <kobysysouvanh@gmail.com>
2023-05-23 06:03:45 -0400

    Slackerbot to calculate current age using go

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/shomali11/slacker"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5309827667476-5292836837831-mbMILlCmBxLNqtvsA24EUAcD")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A059DUZQD17-5300742472742-877349db2b86bca27e9481352977887bb36f2e9db9dac61a7c9e484568975f38")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
+		Description: "yob calculator",
+		Examples:    []string{"my yob is 2020"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				println("error")
+			}
+			age := time.Now().Year() - yob
+			r := fmt.Sprintf("your age is %d", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
